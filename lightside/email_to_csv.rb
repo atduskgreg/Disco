26cc76b7ea0cc3e138da8fa23dc0be5f120b5055
Greg Borenstein <greg.borenstein@gmail.com>
2013-11-17 17:49:55 -0500

    lightside explorations

--- lightside/email_to_csv.rb
+++ lightside/email_to_csv.rb
@@ -1,30 +1,29 @@
 require 'mail'
 require 'csv'
 
-ROOT_PATH = "/Users/greg/Documents/mit/scifi/lawyer/code/disco/data/emails/cuilla-m/"
+ROOT_PATH = "/Users/greg/Documents/mit/scifi/lawyer/enron_mail_20110402/maildir/cuilla-m"
 
 labels = {}
 CSV.foreach("labels.csv") do |row|
 	labels[row[1]] = row[0].to_i
 end
 
-puts labels.inspect
+addresses = open("addresses.csv").read.split("\n")
 
 CSV.open("cuilla_data.csv", "wb") do |csv|
-	csv << ["label", "body", "subject"]
+	csv << ["label", "time", "from", "subject", "body"]
 
-	mails = Dir.entries(ROOT_PATH + "/sent").select{|f| f =~ /[^\.]/ && f != "entities"}
+	mails = Dir.glob(ROOT_PATH + "/**/*.")
 	mails.each_with_index do |filename, i|
 		puts "#{i+1}/#{mails.length}"
-		mail_path = ROOT_PATH + "sent/" + filename
-		mail = Mail.read(mail_path)
+		mail = Mail.read(filename)
 
-		label = labels[mail_path]
+		label = labels[filename]
 		body = mail.body.decoded
 		subject = mail.subject
+		time = mail.date.to_time.to_i
+		from = mail.from[0]
 
-		csv << [label, body, subject]
-
+		csv << [label, time, from, subject, body]
 	end
-
 end
